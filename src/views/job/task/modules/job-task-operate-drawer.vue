488dec578bed31bc9d29d6e62b69f4f51a4da56a
dhb52 <dhb52@126.com>
2024-05-22 16:10:11 +0800

    feat: 新建job，组名、状态各一行

--- src/views/job/task/modules/job-task-operate-drawer.vue
+++ src/views/job/task/modules/job-task-operate-drawer.vue
@@ -281,27 +281,21 @@ watch(visible, () => {
           :placeholder="$t('page.jobTask.form.jobName')"
         />
       </NFormItem>
-      <NGrid cols="2 s:1 m:2" responsive="screen" x-gap="20">
-        <NGi>
-          <NFormItem :label="$t('page.jobTask.groupName')" path="groupName">
-            <SelectGroup v-model:value="model.groupName" />
-          </NFormItem>
-        </NGi>
-        <NGi>
-          <NFormItem :label="$t('page.jobTask.jobStatus')" path="jobStatus">
-            <NRadioGroup v-model:value="model.jobStatus" name="jobStatus">
-              <NSpace>
-                <NRadio
-                  v-for="item in enableStatusNumberOptions"
-                  :key="item.value"
-                  :value="item.value"
-                  :label="$t(item.label)"
-                />
-              </NSpace>
-            </NRadioGroup>
-          </NFormItem>
-        </NGi>
-      </NGrid>
+      <NFormItem :label="$t('page.jobTask.groupName')" path="groupName">
+        <SelectGroup v-model:value="model.groupName" />
+      </NFormItem>
+      <NFormItem :label="$t('page.jobTask.jobStatus')" path="jobStatus">
+        <NRadioGroup v-model:value="model.jobStatus" name="jobStatus">
+          <NSpace>
+            <NRadio
+              v-for="item in enableStatusNumberOptions"
+              :key="item.value"
+              :value="item.value"
+              :label="$t(item.label)"
+            />
+          </NSpace>
+        </NRadioGroup>
+      </NFormItem>
       <NGrid cols="2 s:1 m:2" responsive="screen" x-gap="20">
         <NGi>
           <NFormItem :label="$t('page.jobTask.executorType')" path="executorType">
