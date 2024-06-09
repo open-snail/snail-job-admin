4ec53d3c35b794776cc86fa8e828aa92158f7eb0
dhb52 <dhb52@126.com>
2024-06-09 15:14:59 +0800

    feat(sj_1.0.0): useTitle 加上站点名

--- src/router/guard/title.ts
+++ src/router/guard/title.ts
@@ -2,12 +2,14 @@ import { useTitle } from '@vueuse/core';
 import type { Router } from 'vue-router';
 import { $t } from '@/locales';
 
+const { VITE_APP_TITLE } = import.meta.env;
+
 export function createDocumentTitleGuard(router: Router) {
   router.afterEach(to => {
     const { i18nKey, title } = to.meta;
 
     const documentTitle = i18nKey ? $t(i18nKey) : title;
 
-    useTitle(documentTitle);
+    useTitle(documentTitle, { titleTemplate: `%s | ${VITE_APP_TITLE}` });
   });
 }
