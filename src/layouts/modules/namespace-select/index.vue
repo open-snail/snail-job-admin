2269456d013da2f5cbb7473915f296fcf2c4b8d0
xlsea <m@xlsea.cn>
2024-06-11 17:29:06 +0800

    style(sj_1.1.0_beta1): 优化空间切换组件样式

--- src/layouts/modules/namespace-select/index.vue
+++ src/layouts/modules/namespace-select/index.vue
@@ -1,10 +1,12 @@
-<script lang="ts" setup>
+<script lang="tsx" setup>
 import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { $t } from '@/locales';
 import { localStg } from '@/utils/storage';
 import { useAppStore } from '@/store/modules/app';
 import { useAuthStore } from '@/store/modules/auth';
+import ButtonIcon from '@/components/custom/button-icon.vue';
+import SvgIcon from '@/components/custom/svg-icon.vue';
 
 defineOptions({
   name: 'NamespaceSelect'
@@ -15,14 +17,13 @@ const appStore = useAppStore();
 const authStore = useAuthStore();
 const namespaceId = ref<string>(localStg.get('namespaceId')!);
 const userInfo = localStg.get('userInfo');
-const selectOptions = computed(() =>
-  userInfo?.namespaceIds.map(item => {
-    return { label: item.name, value: item.uniqueId };
-  })
-);
+
 const dropOptions = computed(() =>
   userInfo?.namespaceIds.map(item => {
-    return { label: item.name, key: item.uniqueId };
+    return {
+      label: () => <span class="block w-120px">{item.name}</span>,
+      key: item.uniqueId
+    };
   })
 );
 
@@ -31,37 +32,66 @@ const onChange = (value: string) => {
   authStore.setNamespaceId(value);
   router.go(0);
 };
+
+const namespaceName = computed(() => {
+  return userInfo?.namespaceIds.filter(item => (item.id = namespaceId.value))[0].name || 'Default';
+});
 </script>
 
 <template>
   <NDropdown v-if="appStore.isMobile" :value="namespaceId" :options="dropOptions" trigger="hover" @select="onChange">
     <div>
       <ButtonIcon :tooltip-content="$t('icon.namespace')" tooltip-placement="left">
-        <SvgIcon icon="oui:app-spaces" />
+        <SvgIcon icon="eos-icons:namespace" />
       </ButtonIcon>
     </div>
   </NDropdown>
-  <template v-else>
-    <NTooltip trigger="hover">
-      <template #trigger>
-        <NSelect
-          v-model:value="namespaceId"
-          class="namespace-select"
-          :options="selectOptions"
-          @update:value="onChange"
-        />
-      </template>
-      {{ $t('icon.namespace') }}
-    </NTooltip>
-  </template>
+
+  <NDropdown v-else :value="namespaceId" :options="dropOptions" trigger="click" @select="onChange">
+    <div class="namespace-select">
+      <ButtonIcon class="w-full" :tooltip-content="$t('icon.namespace')" tooltip-placement="left">
+        <SvgIcon icon="eos-icons:namespace" />
+        <NEllipsis class="text-14px">{{ namespaceName }}</NEllipsis>
+        <SvgIcon icon="material-symbols:expand-more-rounded" />
+      </ButtonIcon>
+    </div>
+  </NDropdown>
 </template>
 
 <style lang="scss" scoped>
 .namespace-select {
   width: 150px;
+  border: 1px solid rgb(224, 224, 230);
+  border-radius: 6px;
 
-  :deep(.n-base-selection) {
-    border-radius: 32px !important;
+  :deep(.n-button):hover {
+    background-color: var(--n-color);
   }
+
+  :deep(.n-ellipsis) {
+    max-width: 96px;
+  }
+
+  :deep(.n-button) {
+    width: 100% !important;
+    padding: 0 5px 0 10px !important;
+  }
+
+  :deep(.n-button__content) {
+    width: 100% !important;
+  }
+
+  :deep(.gap-8px) {
+    gap: 0 !important;
+  }
+
+  :deep(.flex-center) {
+    width: 100% !important;
+    justify-content: space-between !important;
+  }
+}
+
+.namespace-select:hover {
+  border-color: rgb(var(--nprogress-color));
 }
 </style>
